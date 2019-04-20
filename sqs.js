60e4c0776a164f3229608f52d3d910efd92dc858
Hugo Sykes <hugo.sykes@infinityworks.com>
2019-04-20 13:53:46 +0100

    Deletes messages once they've been received and written

--- sqs.js
+++ sqs.js
@@ -1,7 +1,7 @@
 const diskWriter = require('./disk-writer')
 
 function drainQueue(outputFile, queueUrl, region, profile) {
-  process.env.AWS_SDK_LOAD_CONFIG=true
+  process.env.AWS_SDK_LOAD_CONFIG = true
   process.env.AWS_PROFILE = profile
   const AWS = require('aws-sdk')
   AWS.config.region = region
@@ -15,16 +15,48 @@ function receiveMessages(queueUrl, sqs, outputFile) {
     QueueUrl: queueUrl,
     MaxNumberOfMessages: 10
   }
-
   sqs.receiveMessage(params, (err, data) => {
-    if (err) console.log(err.stack)
-    else diskWriter.writeToDisk(outputFile, stringify(data))
+    if (err) {
+      console.log(err.stack)
+      return err
+    } else {
+      const {Messages} = data
+      diskWriter.writeToDisk(outputFile, getBody(Messages))
+      deleteMessages(queueUrl, sqs, Messages)
+    }
   })
 }
 
-function stringify(res) {
-  return res.Messages.map(m => {
+function deleteMessages(queueUrl, sqs, messages) {
+  const params = {
+    QueueUrl: queueUrl,
+    Entries: getReceiptHandles(messages)
+  }
+  sqs.deleteMessageBatch(params, (err, data) => {
+    if (err) {
+      console.log(err)
+    } else {
+      if (data.Failed.length !== 0) {
+        data.Failed.forEach(m => {
+          console.log(`Failed to delete: ${m.Id}, for reason: ${m.Message}`
+          + `, and code ${m.Code}`)
+        })
+      }
+      data.Successful.forEach(s => console.log(`Successfully deleted `
+      + `${s.Id}`))
+    }
+  })
+}
+
+function getBody(messages) {
+  return messages.map(m => {
     return m.Body
+  })
+}
+
+function getReceiptHandles(messages) {
+  return messages.map(m => {
+    return {Id: m.MessageId, ReceiptHandle: m.ReceiptHandle}
   })
 }
 
